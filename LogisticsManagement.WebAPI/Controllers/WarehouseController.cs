4d103b2e57263e0beabdcd617e2a537559140e97
Nisargkumar Naginbhai Patel <nisargkumarp@cybage.com>
2024-04-09 18:49:41 +0530

    Add project files.

---
+++ LogisticsManagement.WebAPI/Controllers/WarehouseController.cs
@@ -0,0 +1,36 @@
+﻿using LogisticsManagement.Service.DTOs;
+using LogisticsManagement.Service.Services.IServices;
+using LogisticsManagement.WebAPI.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LogisticsManagement.WebAPI.Controllers
+{
+    [Route("api/warehouse")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+        public WarehouseController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWarehouses()
+        {
+            try
+            {
+                List<WarehouseDTO> warehouses= await _adminService.GetAllWarehousesAsync();
+
+                return Ok(warehouses);
+
+
+            }catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ApiResponseHelper.Response(false, HttpStatusCode.InternalServerError, null, ex.Message));
+            }
+        }
+    }
+}
